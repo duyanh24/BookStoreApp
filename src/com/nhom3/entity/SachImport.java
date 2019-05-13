42ee692ec3208f08d26c00f5da5b90baeddb02ed
Duy Anh Le <42759980+duyanh24@users.noreply.github.com>
2019-05-13 21:17:20 +0700

    class BookImport

---
+++ src/com/nhom3/entity/SachImport.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nhom3.entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class SachImport {
+    private String MaTheLoai;
+    private String TieuDe;
+    private String TenTacGia;
+    private String NhaXB;
+    private String SoLuong;
+    private String GiaNhap;
+    private String GiaBan;
+    private String GioiThieuNgan;
+
+    public String getMaTheLoai() {
+        return MaTheLoai;
+    }
+
+    public void setMaTheLoai(String MaTheLoai) {
+        this.MaTheLoai = MaTheLoai;
+    }
+
+    public String getTieuDe() {
+        return TieuDe;
+    }
+
+    public void setTieuDe(String TieuDe) {
+        this.TieuDe = TieuDe;
+    }
+
+    public String getTenTacGia() {
+        return TenTacGia;
+    }
+
+    public void setTenTacGia(String TenTacGia) {
+        this.TenTacGia = TenTacGia;
+    }
+
+    public String getNhaXB() {
+        return NhaXB;
+    }
+
+    public void setNhaXB(String NhaXB) {
+        this.NhaXB = NhaXB;
+    }
+
+    public String getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(String SoLuong) {
+        this.SoLuong = SoLuong;
+    }
+
+    public String getGiaNhap() {
+        return GiaNhap;
+    }
+
+    public void setGiaNhap(String GiaNhap) {
+        this.GiaNhap = GiaNhap;
+    }
+
+    public String getGiaBan() {
+        return GiaBan;
+    }
+
+    public void setGiaBan(String GiaBan) {
+        this.GiaBan = GiaBan;
+    }
+
+    public String getGioiThieuNgan() {
+        return GioiThieuNgan;
+    }
+
+    public void setGioiThieuNgan(String GioiThieuNgan) {
+        this.GioiThieuNgan = GioiThieuNgan;
+    }
+}
